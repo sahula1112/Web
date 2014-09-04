c8679aa5e0ab2449d44db51414a9b7a61d947e23
tanniiz <tAnniiz@Pichests-MacBook-Air.local>
2014-09-04 23:19:06 +0700

    inbox

--- Source/nurse/inbox.php
+++ Source/nurse/inbox.php
@@ -113,7 +113,7 @@
 
                     }
                     else{
-                      echo "<td><a href='http://localhost/Web/Source/nurse/reply.php?messageid=". $row['msgid']. "&reciever=" . $row['sender'] . "'><button class='btn btn-danger'>Wating for reply</button></a></td>";
+                      echo "<td><a href='http://localhost/Web/Source/nurse/reply.php?messageid=". $row['msgid'] . "'><button class='btn btn-danger'>Wating for reply</button></a></td>";
                     }
                     echo "</tr>";
                     echo "</tbody>";
